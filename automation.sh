6128ac707c4bf0e608794914a6b74594093a9774
chaithanya1812 <bob@example.com>
2024-10-11 14:45:04 +0530

    terraform1

--- automation.sh
+++ automation.sh
@@ -1,1 +1,17 @@
-ansible-playbook -i
+pipeline{
+    agent any
+    stages{
+        stage('one'){
+            steps{
+                script{
+                  withVault(configuration: [disableChildPoliciesOverride: false, engineVersion: 2, skipSslVerification: true, timeout: 60, vaultCredentialId: 'app-role', vaultUrl: 'http://3.86.34.246:8200'], vaultSecrets: [[path: 'roboshop-dev/cart', secretValues: [[envVar: 'sample', vaultKey: 'name']]]]) {
+                      echo "Thiis os $sample"
+                  }
+                }
+            }
+        }
+    }
+}
+
+ lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+ lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null )
